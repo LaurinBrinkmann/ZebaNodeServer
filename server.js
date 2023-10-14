32d6516537205f931588cc31b9a092774243c8b7
Laurin Brinkmann <l.brinkmann123@gmail.com>
2023-10-14 15:09:00 +0200

    configured endpoints

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
-const cors = require("cors");
 const fs = require("fs");
+const cors = require("cors");
 const port = 8080;
-const filename = "/scannedCodes.json";
+const filename = __dirname + "/scannedCodes.json";
 
 //Middleware
 app.use(express.json()); //for parsing application/json
@@ -16,25 +16,24 @@ app.use(log);
 
 
 //Endpoints
-app.get("/", function (req, res) {
-    return res.send("Hello World!");
-});
-
-app.get("/api/history", function (req, res) {
+app.get("/list", function (req, res) {
     fs.readFile(filename, "utf8", function (err, data) {
-        const dataAsObject = JSON.parse(data)[req.params.id];
         res.writeHead(200, {
             "Content-Type": "application/json",
         });
-        res.end(JSON.stringify(dataAsObject));
+        res.end(data);
     });
 });
 
-app.put("/api/add", function (req, res) {
+
+app.post("/add", function (req, res) {
     fs.readFile(filename, "utf8", function (err, data) {
         let dataAsObject = JSON.parse(data);
-        dataAsObject[req.params.id].name = req.body.name;
-        dataAsObject[req.params.id].rating = req.body.rating;
+        dataAsObject.push({
+            id: dataAsObject.length,
+            name: req.body.name,
+            rating: req.body.rating,
+        });
         fs.writeFile(filename, JSON.stringify(dataAsObject), () => {
             res.writeHead(200, {
                 "Content-Type": "application/json",
@@ -43,19 +42,4 @@ app.put("/api/add", function (req, res) {
         });
     });
 });
-
-app.delete("/api/delete/:id", function (req, res) {
-    fs.readFile(filename, "utf8", function (err, data) {
-        let dataAsObject = JSON.parse(data);
-        dataAsObject.splice(req.params.id, 1);
-        fs.writeFile(filename, JSON.stringify(dataAsObject), () => {
-            res.writeHead(200, {
-                "Content-Type": "application/json",
-            });
-            res.end(JSON.stringify(dataAsObject));
-        });
-    });
-});
-
-
 app.listen(port, () => console.log(`Server listening on port ${port}!`));
